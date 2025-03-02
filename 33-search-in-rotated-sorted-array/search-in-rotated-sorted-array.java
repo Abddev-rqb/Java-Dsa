2e88f4c5bc598ad65f49669e8892a24441cc962f
Abddev-rqb <abdulch530@gmail.com>
2025-03-02 10:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (51.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,50 +1,36 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int peak = peakE(nums);
-        if (peak == -1){
-            return binS(nums, target, 0, nums.length-1);
-        }
-        if(nums[peak]== target){
-            return peak;
-        }
-        if (target >= nums[0]){
-            return binS(nums, target, 0, peak-1);
-        }
-        return binS(nums, target, peak+1, nums.length-1);
+    public int search(int[] arr, int target) {
+       int peak  = pivot(arr);
+       if (peak == -1){ return binS(arr, 0, arr.length-1, target);}
+       if (arr[peak] == target){return peak;}
+       if (arr[0] <= target){return binS(arr, 0, peak-1, target);}
+       return binS(arr, peak+1, arr.length-1, target);
     }
-
-    public int binS(int[] nums, int target, int start, int end){
-        while(start <= end){
+    public int binS(int[] arr, int start, int end, int target){
+        while (start <= end){
             int mid = start + (end - start)/2;
-            if (nums[mid] == target){
+            if (arr[mid] == target){
                 return mid;
-            }
-            if (nums[mid] > target){
-                end = mid-1;
-            }else{
-                start = mid+1;
-            }
-        }
-        return -1;
-    }
-
-    
-    public int peakE(int[] nums){
-        int start = 0, end = nums.length-1;
-        while(start < end) {
-            int mid = start + (end - start)/2;
-            if (mid < end && nums[mid] > nums[mid+1]){
-                return mid;
-            }
-            if (start < mid && nums[mid-1] > nums[mid]){
-                return mid-1;
-            }
-            if (nums[start] <= nums[mid]){
+            }else if (arr[mid] < target){
                 start = mid+1;
             }else{
                 end = mid-1;
             }
         }
         return -1;
+    }
+    public int pivot(int[] arr){
+        int start =0, end = arr.length-1;
+        while (start < end){
+            int mid = start + (end - start)/2;
+            if (end > mid && arr[mid] > arr[mid+1]){return mid;}
+            if (start < mid && arr[mid] < arr[mid-1]){ end = mid-1;}
+            if (arr[start] <= arr[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return start;
     }
 }
